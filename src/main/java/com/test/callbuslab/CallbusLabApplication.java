e373b365bfd445db5d8b74a5791f69407bb81891
이승민 <sml7179@naver.com>
2022-01-19 11:42:15 +0900

    Initial commit

---
+++ src/main/java/com/test/callbuslab/CallbusLabApplication.java
@@ -0,0 +1,13 @@
+package com.test.callbuslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallbusLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CallbusLabApplication.class, args);
+    }
+
+}
