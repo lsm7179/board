5abbc13ebba4fd3b1fcaa20d029a4deef86d712a
이승민 <sml7179@naver.com>
2022-06-16 22:59:43 +0900

    test: 검증 변경

--- src/test/java/study/ListSubListTest.java
+++ src/test/java/study/ListSubListTest.java
@@ -7,6 +7,8 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class ListSubListTest {
 
     @Test
@@ -14,9 +16,9 @@ public class ListSubListTest {
 
         List<Integer> test2 = Arrays.asList(1);
         List<Integer> list1 = test2.subList(0, test2.size()-1);
-        Integer a = null;
-        a = test2.get(test2.size() -1);
-        System.out.println(list1);
-        System.out.println(a);
+        Integer a = test2.get(test2.size() -1);
+
+        assertThat(list1).isEqualTo(Collections.EMPTY_LIST);
+        assertThat(a).isEqualTo(1);
     }
 }
